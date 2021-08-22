81d2178ab572161e74ce3e76c5e54fea8c2d16a7
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2021-08-23 00:35:53 +0200

    Inject TeamdeckApiClient to all resolvers

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,21 @@
-mod timer;
-mod teamdeck;
+mod project;
 mod resource;
+mod teamdeck;
+mod timer;
 
-use crate::timer::{TimerQuery};
-use async_graphql::{EmptyMutation, EmptySubscription, MergedObject, Schema};
+use crate::project::ProjectQuery;
 use crate::resource::ResourceQuery;
+use crate::teamdeck::api::TeamdeckApiClient;
+use crate::timer::TimerQuery;
+use async_graphql::{EmptyMutation, EmptySubscription, MergedObject, Schema};
 
 pub type ApiSchema = Schema<QueryRoot, EmptyMutation, EmptySubscription>;
 
 #[derive(MergedObject, Default)]
-pub struct QueryRoot(TimerQuery, ResourceQuery);
+pub struct QueryRoot(TimerQuery, ResourceQuery, ProjectQuery);
 
 pub fn create_schema() -> ApiSchema {
-    Schema::new(QueryRoot::default(), EmptyMutation, EmptySubscription)
+    Schema::build(QueryRoot::default(), EmptyMutation, EmptySubscription)
+        .data(TeamdeckApiClient::default())
+        .finish()
 }
