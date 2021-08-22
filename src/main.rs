ef1b1833f1656ee1122de1ec9746d8c8e39c25e9
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2021-08-22 10:25:19 +0200

    Update main.rs file with starting rocket api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+use rocket::{Rocket, Build};
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> Rocket<Build> {
+    rocket::build().mount("/", routes![index])
 }
