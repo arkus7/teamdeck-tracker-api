b32927cfa05b7db97969a98738939404560f9df9
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2021-08-22 20:30:09 +0200

    Update root query

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
-use async_graphql::{EmptyMutation, EmptySubscription, Schema, Object, Context};
+mod timer;
+
+use async_graphql::{EmptyMutation, EmptySubscription, Schema, Object, Context, Result, MergedObject};
+use crate::timer::{Timer, TimerQuery};
 
 pub type ApiSchema = Schema<QueryRoot, EmptyMutation, EmptySubscription>;
 
-pub struct QueryRoot;
+#[derive(MergedObject, Default)]
+struct QueryRoot(TimerQuery);
 
-#[Object]
-impl QueryRoot {
-    async fn hello_world(&self) -> &str {
-        "Hello, World"
-    }
+pub fn create_schema() -> ApiSchema {
+    Schema::new(QueryRoot::default(), EmptyMutation, EmptySubscription)
 }
