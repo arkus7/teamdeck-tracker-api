a04a336dbbe696179fa83ea6e57c67dbe58e1ae5
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2021-08-22 20:30:26 +0200

    Use function to create schema from lib.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use actix_web::{guard, web, App, HttpResponse, HttpServer, Result};
 use async_graphql::{
     http::{playground_source, GraphQLPlaygroundConfig}, EmptyMutation, EmptySubscription, Schema};
 use async_graphql_actix_web::{Request, Response};
-use teamdeck_tracker_api::{QueryRoot, ApiSchema};
+use teamdeck_tracker_api::{QueryRoot, ApiSchema, create_schema};
 
 async fn index(schema: web::Data<ApiSchema>, req: Request) -> Response {
     schema.execute(req.into_inner()).await.into()
@@ -19,14 +19,11 @@ async fn index_playground() -> Result<HttpResponse> {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription)
-        .finish();
-
     println!("Playground: http://localhost:8000");
 
     HttpServer::new(move || {
         App::new()
-            .data(schema.clone())
+            .data(create_schema().clone())
             .service(web::resource("/").guard(guard::Post()).to(index))
             .service(web::resource("/").guard(guard::Get()).to(index_playground))
     })
