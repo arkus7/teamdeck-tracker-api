7a070d4006cc501bf53886da2514d5a2d1cd7a9e
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2021-08-22 10:51:36 +0200

    Update main function

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,36 @@
-#[macro_use] extern crate rocket;
 
-use rocket::{Rocket, Build};
+use actix_web::{guard, web, App, HttpResponse, HttpServer, Result};
+use async_graphql::{
+    http::{playground_source, GraphQLPlaygroundConfig}, EmptyMutation, EmptySubscription, Schema};
+use async_graphql_actix_web::{Request, Response};
+use teamdeck_tracker_api::{QueryRoot, ApiSchema};
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+async fn index(schema: web::Data<ApiSchema>, req: Request) -> Response {
+    schema.execute(req.into_inner()).await.into()
 }
 
-#[launch]
-fn rocket() -> Rocket<Build> {
-    rocket::build().mount("/", routes![index])
+async fn index_playground() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(playground_source(
+            GraphQLPlaygroundConfig::new("/").subscription_endpoint("/"),
+        )))
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription)
+        .finish();
+
+    println!("Playground: http://localhost:8000");
+
+    HttpServer::new(move || {
+        App::new()
+            .data(schema.clone())
+            .service(web::resource("/").guard(guard::Post()).to(index))
+            .service(web::resource("/").guard(guard::Get()).to(index_playground))
+    })
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
