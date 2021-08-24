0f8807466bda6f996705937588a22950830a76ab
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2021-08-24 23:29:57 +0200

    Add logger to app

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+mod telemetry;
+
 use actix_web::web::Data;
 use actix_web::{guard, web, App, HttpResponse, HttpServer, Result};
 use async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
 use async_graphql_actix_web::{Request, Response};
 use teamdeck_tracker_api::{create_schema, ApiSchema};
+use crate::telemetry::{get_logs_subscriber, init_logs_subscriber};
+use tracing_actix_web::{TracingLogger, TracingLoggerMiddleware};
 
 async fn index(schema: web::Data<ApiSchema>, req: Request) -> Response {
     schema.execute(req.into_inner()).await.into()
@@ -18,10 +22,14 @@ async fn index_playground() -> Result<HttpResponse> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let logs_subscriber = get_logs_subscriber("TeamdeckTimerAPI".into(), "info".into(), std::io::stdout);
+    init_logs_subscriber(logs_subscriber);
+
     println!("Playground: http://localhost:8000");
 
     HttpServer::new(move || {
         App::new()
+            .wrap(TracingLogger)
             .app_data(Data::new(create_schema().clone()))
             .service(web::resource("/").guard(guard::Post()).to(index))
             .service(web::resource("/").guard(guard::Get()).to(index_playground))
