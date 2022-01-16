a074dea7040e04aeec74ef21ffd66714364f9667
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2022-01-16 15:50:57 +0100

    Implement creating access/refresh token

--- src/auth.rs
+++ src/auth.rs
@@ -1,6 +1,8 @@
 use async_graphql::{Context, Object, Result, SimpleObject};
 use serde::Serialize;
 
+use crate::teamdeck::{api::TeamdeckApiClient, error::TeamdeckApiError};
+
 mod google {
     use serde::Deserialize;
     use thiserror::Error;
@@ -125,6 +127,59 @@ mod google {
     }
 }
 
+mod token {
+    use async_graphql::SimpleObject;
+    use jsonwebtoken::{encode, EncodingKey, Header};
+    use serde::{Deserialize, Serialize};
+    use thiserror::Error;
+
+    const JWT_ACCESS_TOKEN_SECRET: &str = "secret";
+    const JWT_REFRESH_TOKEN_SECRET: &str = "secret";
+
+    #[derive(Debug, Serialize, Deserialize)]
+    struct Claims {
+        sub: String,
+        resource_id: u64,
+    }
+
+    #[derive(SimpleObject, Debug, Serialize)]
+    pub struct Token {
+        access_token: String,
+        refresh_token: String,
+        expires_in: u64,
+    }
+
+    #[derive(Debug, Error)]
+    pub enum TokenError {
+        #[error("unknown error occured during token creation")]
+        Unknown,
+    }
+
+    impl Token {
+        pub fn with_user_data(email: &str, resource_id: u64) -> Result<Self, TokenError> {
+            let claims = Claims {
+                sub: email.to_string(),
+                resource_id,
+            };
+
+            let headers = Header::default();
+            let access_token_key = EncodingKey::from_secret(JWT_ACCESS_TOKEN_SECRET.as_ref());
+            let refresh_token_key = EncodingKey::from_secret(JWT_REFRESH_TOKEN_SECRET.as_ref());
+
+            let access_token =
+                encode(&headers, &claims, &access_token_key).map_err(|e| TokenError::Unknown)?;
+            let refresh_token =
+                encode(&headers, &claims, &refresh_token_key).map_err(|e| TokenError::Unknown)?;
+
+            Ok(Token {
+                access_token,
+                refresh_token,
+                expires_in: 0,
+            })
+        }
+    }
+}
+
 #[derive(Default, Debug)]
 pub struct AuthQuery;
 
@@ -139,29 +194,23 @@ impl AuthQuery {
 #[derive(Default, Debug)]
 pub struct AuthMutation;
 
-#[derive(SimpleObject, Debug, Serialize)]
-struct Token {
-    access_token: String,
-    refresh_token: String,
-    expires_in: u64,
-}
-
 #[Object]
 impl AuthMutation {
-    async fn login_with_google(&self, ctx: &Context<'_>, code: String) -> Result<Token> {
+    async fn login_with_google(&self, ctx: &Context<'_>, code: String) -> Result<token::Token> {
         let google_token = google::GoogleOAuth2::exchange_code_for_token(code).await?;
 
         dbg!(&google_token);
         let email = google_token.email()?;
 
         // TODO: Fetch `resource_id` from Teamdeck API for that email
-        // TODO: Implement creating new token based on `email` and `resource_id`
+        let teamdeck = ctx.data_unchecked::<TeamdeckApiClient>();
+        let resource = teamdeck.get_resource_by_email(&email).await?;
 
-        let token = Token {
-            access_token: "".to_string(),
-            refresh_token: "".to_string(),
-            expires_in: 0,
-        };
-        Ok(token)
+        if let Some(resource) = resource {
+            let token = token::Token::with_user_data(&email, resource.id)?;
+            Ok(token)
+        } else {
+            Err(async_graphql::Error::new(format!("No Teamdeck account found with `{}` email", email)))
+        }
     }
 }
